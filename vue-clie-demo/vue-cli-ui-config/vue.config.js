d200e7351155d18064b9f9c27bbd4cf2e605855a
jianghuisheng <1697965700@qq.com>
2021-03-04 15:28:39 +0800

    1

--- vue-clie-demo/vue-cli-ui-config/vue.config.js
+++ vue-clie-demo/vue-cli-ui-config/vue.config.js
@@ -4,7 +4,7 @@ const seller = require('./data/seller.json');
 
 module.exports = {
     // baseUrl: '/', // 根路径
-    publicPath: '/',
+    publicPath: './',
     outputDir: "dist", // 构建输出目录
     assetsDir: 'assets', // 静态资源目录（js,css,img,fonts）
     lintOnSave: false, // 是否开启eslint保存检测，有效值：true || false || 'error'
